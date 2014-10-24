b2d93fb40294880ab3758f4fbe79e170fb4650ff
Samantha Stallings <samantha.stallings1@gmail.com>
2014-10-24 15:00:34 -0700

    clear text field

--- portfolio/twooter/twitta.js
+++ portfolio/twooter/twitta.js
@@ -1,11 +1,18 @@
  var url = 'https://sweltering-inferno-6192.firebaseio.com';
     var firebaseRef = new Firebase(url);
 
-    function funct1(event)
-    {
+    function funct1(event) {
         event.preventDefault();
         var tweet_content = $("#user-input").val();
         firebaseRef.push({tweet_text: tweet_content});
     }
 
+    function clearInput() {
+      $('#user-input').val('');
+    }
+
+
+  //----- Event handlers -----//
+
+    $('.submit').click(clearInput);
     $('.submit').click(funct1);
